77efbd6b19e4ea7db625034e6c66f351df63bfff
Ronak25034 <ronakagarwal0181@gmail.com>
2026-03-28 15:15:04 +0530

    Updated app.py

--- Lab/Experiment7/my-app/app.py
+++ Lab/Experiment7/my-app/app.py
@@ -1,22 +1,10 @@
-from flask import Flask, jsonify
-import datetime
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to my updated CI/CD Pipeline!"
+    return "Hello from CI/CD Pipeline!"
+    #return "Hello from CI/CD Pipeline!, my sapid is 123456"
 
-@app.route("/status")
-def status():
-    return jsonify({
-        "status": "running",
-        "time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    })
+app.run(host="0.0.0.0", port=80)
 
-@app.route("/user/<name>")
-def greet_user(name):
-    return f"Hello, {name}! Your CI/CD pipeline is working perfectly."
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
