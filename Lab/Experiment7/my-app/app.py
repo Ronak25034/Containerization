0dba310954892094e48f5ea2b32bf6aa91a23442
Ronak25034 <ronakagarwal0181@gmail.com>
2026-03-28 15:32:39 +0530

    Updated app.py

--- Lab/Experiment7/my-app/app.py
+++ Lab/Experiment7/my-app/app.py
@@ -1,14 +1,10 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from CI/CD Pipeline - Version 2!"
+    return "Hello from CI/CD Pipeline!"
+    #return "Hello from CI/CD Pipeline!, my sapid is 123456"
 
-@app.route("/health")
-def health():
-    return "App is running successfully."
+app.run(host="0.0.0.0", port=80)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
